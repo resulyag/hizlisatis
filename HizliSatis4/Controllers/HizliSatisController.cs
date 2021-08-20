53a433dfa6cb82f0f7d16b27649ba37c5f43c5de
resulyag <resulyag@gmail.com>
2021-08-20 11:58:25 +0300

    init

---
+++ HizliSatis4/Controllers/HizliSatisController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace HizliSatis4.Controllers
+{
+    public class HizliSatisController : Controller
+    {
+        // GET: HizliSatis
+        public ActionResult Index()
+        {
+            return View();
+        }
+            //[HttpPost]
+            //public JsonResult Index(Models.Email model)
+            //{
+            //    MailMessage mailim = new MailMessage();
+            //    mailim.To.Add("bilgi@bilsoft.com");
+            //    mailim.From = new MailAddress("bilgi@bilsoft.com");
+            //    mailim.Subject = "Bize Ulaşın Sayfasından Mesajınız Var. ";
+            //    mailim.Body = "Ad:" + model.name + " <br> Mail:" + " " + model.email + " <br>Konu:" + model.subject + " <br>Mesaj:" + model.message;
+            //    mailim.IsBodyHtml = true;
+
+            //    SmtpClient smtp = new SmtpClient();
+            //    smtp.Credentials = new NetworkCredential("bilgi@bilsoft.com", "#sifre");
+            //    smtp.Port = 587;
+            //    smtp.Host = "smtp.#.com";
+            //    smtp.EnableSsl = false;
+
+            //    try
+            //    {
+            //        smtp.Send(mailim);
+            //    }
+            //    catch (Exception)
+            //    {
+
+            //    }
+
+            //    return Json("asdasd");
+            //}
+
+            public ActionResult SiparisVeTeklif()
+            {
+                return View();
+            }
+            public ActionResult OtomatikYedekleme()
+            {
+                return View();
+            }
+            public ActionResult ExcelVeriAktarim()
+            {
+                return View();
+            }
+            public ActionResult StokSayim()
+            {
+                return View();
+            }
+            public ActionResult StokRiskLimiti()
+            {
+                return View();
+            }
+            public ActionResult Ozellikler()
+            {
+                return View();
+            }
+        }
+    }
+
---
+++ HizliSatis4/Models/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HizliSatis4.Models
+{
+    public class Email
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+    }
+
+    public class Message
+    {
+        public bool IsSuccess { get; set; }
+        public bool message { get; set; }
+    }
+}
