790c9786d26fb4444eb9d18ef80c8bef5ba4541f
resulyag <resulyag@gmail.com>
2021-08-23 14:11:24 +0300

    iletişim sayfası eklendi ana sayfadaki butonlar düzenlendi

--- HizliSatis4/Controllers/HizliSatisController.cs
+++ HizliSatis4/Controllers/HizliSatisController.cs
@@ -15,58 +15,62 @@ namespace HizliSatis4.Controllers
         {
             return View();
         }
-            //[HttpPost]
-            //public JsonResult Index(Models.Email model)
-            //{
-            //    MailMessage mailim = new MailMessage();
-            //    mailim.To.Add("bilgi@bilsoft.com");
-            //    mailim.From = new MailAddress("bilgi@bilsoft.com");
-            //    mailim.Subject = "Bize Ulaşın Sayfasından Mesajınız Var. ";
-            //    mailim.Body = "Ad:" + model.name + " <br> Mail:" + " " + model.email + " <br>Konu:" + model.subject + " <br>Mesaj:" + model.message;
-            //    mailim.IsBodyHtml = true;
+        //[HttpPost]
+        //public JsonResult Index(Models.Email model)
+        //{
+        //    MailMessage mailim = new MailMessage();
+        //    mailim.To.Add("bilgi@bilsoft.com");
+        //    mailim.From = new MailAddress("bilgi@bilsoft.com");
+        //    mailim.Subject = "Bize Ulaşın Sayfasından Mesajınız Var. ";
+        //    mailim.Body = "Ad:" + model.name + " <br> Mail:" + " " + model.email + " <br>Konu:" + model.subject + " <br>Mesaj:" + model.message;
+        //    mailim.IsBodyHtml = true;
 
-            //    SmtpClient smtp = new SmtpClient();
-            //    smtp.Credentials = new NetworkCredential("bilgi@bilsoft.com", "#sifre");
-            //    smtp.Port = 587;
-            //    smtp.Host = "smtp.#.com";
-            //    smtp.EnableSsl = false;
+        //    SmtpClient smtp = new SmtpClient();
+        //    smtp.Credentials = new NetworkCredential("bilgi@bilsoft.com", "#sifre");
+        //    smtp.Port = 587;
+        //    smtp.Host = "smtp.#.com";
+        //    smtp.EnableSsl = false;
 
-            //    try
-            //    {
-            //        smtp.Send(mailim);
-            //    }
-            //    catch (Exception)
-            //    {
+        //    try
+        //    {
+        //        smtp.Send(mailim);
+        //    }
+        //    catch (Exception)
+        //    {
 
-            //    }
+        //    }
 
-            //    return Json("asdasd");
-            //}
+        //    return Json("asdasd");
+        //}
 
-            public ActionResult SiparisVeTeklif()
-            {
-                return View();
-            }
-            public ActionResult OtomatikYedekleme()
-            {
-                return View();
-            }
-            public ActionResult ExcelVeriAktarim()
-            {
-                return View();
-            }
-            public ActionResult StokSayim()
-            {
-                return View();
-            }
-            public ActionResult StokRiskLimiti()
-            {
-                return View();
-            }
-            public ActionResult Ozellikler()
-            {
-                return View();
-            }
+        public ActionResult SiparisVeTeklif()
+        {
+            return View();
+        }
+        public ActionResult OtomatikYedekleme()
+        {
+            return View();
+        }
+        public ActionResult ExcelVeriAktarim()
+        {
+            return View();
+        }
+        public ActionResult StokSayim()
+        {
+            return View();
+        }
+        public ActionResult StokRiskLimiti()
+        {
+            return View();
+        }
+        public ActionResult Ozellikler()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
         }
     }
+}
 
